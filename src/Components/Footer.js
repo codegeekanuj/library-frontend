ccbd9eb5a0b867c905c71e1fa393249ff556955f
anuj tripathi <atripathi1253@gmail.com>
2023-12-03 10:44:36 +0530

    code updated

---
+++ src/Components/Footer.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Footer.css'
+
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div>
+                <div className='footer-data'>
+                    <div className="contact-details">
+                        <h1>Contact Us</h1>
+                        <p>Librarian</p>
+                        <p>Dayalbagh Educational Institute</p>
+                        <p>Agra-2201655</p>
+                        <p>Uttar Pradest</p>
+                        <p>India</p>
+                        <p><b>Email:</b></p>
+                    </div>
+                    <div className='usefull-links'>
+                        <h1>Usefull Links</h1>
+                        <a href='#home'>Link-1</a>
+                        <a href='#home'>Link-1</a>
+                        <a href='#home'>Link-1</a>
+                        <a href='#home'>Link-1</a>
+                    </div>
+                    <div className='librarian-details'>
+                        <h1>Librarian</h1>
+                        <p>Name</p>
+                        <p>Education</p>
+                        <p>Contact: +91 9123456787</p>
+                    </div>
+                </div>
+                <div className="contact-social" >
+                    <a href='#home' className='social-icon'><TwitterIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='#home' className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='#home' className='social-icon'><TelegramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='#home' className='social-icon'><InstagramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                </div>
+            </div>
+            <div className='copyright-details'>
+                <p className='footer-copyright'>&#169; 2020 copyright all right reserved<br /><span>Designed with ❤️ by Pranavdhar Reddy</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
